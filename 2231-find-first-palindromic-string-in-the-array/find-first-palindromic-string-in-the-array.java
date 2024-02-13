45235a06e4a403959f8b52eb72a8d4968b7c1da7
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-02-13 21:03:34 +0530

    Update find-first-palindromic-string-in-the-array.java

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,11 +1,24 @@
-public class Solution {
-    public static String firstPalindrome(String[] words) {
-        for (String word : words) {
-            StringBuilder reversed = new StringBuilder(word).reverse();
-            if (word.equals(reversed.toString())) {
-                return word;
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String s:words)
+        {
+            if (ispalindrome (s))
+            {
+                return s;
             }
         }
         return "";
+        
+    }
+    public static boolean ispalindrome(String s){
+
+        for(int i=0;i<s.length();i++)
+        {
+            if (s.charAt(i)!=s.charAt(s.length()-1-i))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
