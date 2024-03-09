bad4d5b02bd9337b283356bbd6bfb343c2ab5d10
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-03-09 18:28:05 +0530

    Time: 2 ms (81.56%) | Memory: 62.9 MB (29.04%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,18 +1,24 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer>set=new HashSet<>();
-        for(int n:nums1)
-        {
-            set.add(n);
-        }
-        for(int n:nums2)
-        {
-            if (set.contains(n))
-            {
-                return n;
-            }
-        }
-        return -1;
+       //second method using two pointer
+       int i=0;
+       int j=0;
+       while(i<nums1.length && j<nums2.length)
+       {
+           if (nums1[i]==nums2[j])
+           {
+               return nums1[i];
+           }
+           else if (nums1[i]<nums2[j])
+           {
+               //if it is smaller then increase its value
+               i++;
+           }
+           else{
+               j++;//its means second array value need to be increased
+           }
+       }
+       return -1;
      
         
     }
