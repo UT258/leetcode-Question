48b02f4b5e0a549a368c84783cec6284c6682b74
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-07-30 21:03:39 +0530

    Time: 140 ms (5.29%) | Memory: 45.5 MB (39.55%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumDeletions(String s) {
+       Stack<Character>st=new Stack<>();
+        int count=0;
+        for (int i = 0; i <s.length(); i++) {
+           
+            if(!st.isEmpty() && st.peek()=='b' && s.charAt(i)=='a')
+            {
+                //if top is you can place a after it
+                st.pop();
+                count++;
+            }
+            else
+            {
+                //if top is a you can place b after it you can just push it
+                st.push(s.charAt(i));
+            }
+            
+        }
+        return count;
+    }
+}
