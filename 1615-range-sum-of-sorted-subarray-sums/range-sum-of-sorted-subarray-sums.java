2244243b80edcb80c63823787c1073edc22b2023
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-08-04 21:42:03 +0530

    Time: 78 ms (40.99%) | Memory: 44.6 MB (82.43%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,21 +1,29 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-         ArrayList<Integer>list=new ArrayList<>();   
-     for (int i = 0; i < nums.length; i++) {
-        int sum=0;
-        for (int j = i; j < nums.length; j++) {
-            sum+=nums[j];
-            list.add(sum);
-           
+        int M=(int) Math.pow(10,9)+7;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+
+        // Initialize the priority queue with the initial subarray sums
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[]{nums[i], i});
         }
-        
-     }
-     Collections.sort(list,(a,b)->a-b);
-        int rsum=0;
-        for (int i = left-1; i < right; i++) {
-            rsum=(rsum+list.get(i))% ((int)Math.pow(10,9)+7);
+
+        int result = 0;
+
+        for (int i = 1; i <= right; i++) {
+            int[] current = pq.poll();
+
+            // If the current index is within the desired range, add the value to the result
+            if (i >= left) {
+                result = (result + current[0]) % M;
+            }
+
+            // If index is less than the last index, increment it and add the new subarray sum to the queue
+            if (current[1] < n - 1) {
+                pq.add(new int[]{current[0] + nums[current[1] + 1], current[1] + 1});
+            }
         }
-        return rsum ;
+        return result;
  
     }
 }
