40a2d185184258fb2c8296a6ef783fb29b11452a
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-08-18 23:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (79.25%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        //using memoization
+        int arr[]=new int [46];
+        Arrays.fill(arr,-1);//to turn all the element to -1
+       return solve(arr,n);
+    }
+    private int solve(int arr[ ],int n)
+
+    {
+        if (n<0)
+        {
+            return 0;
+        }
+        if(n==0)
+        {
+            return 1;//found a way
+        }
+        if(arr[n]!=-1)
+        {
+            return arr[n]; //if it is not -1 then it means it already calulated so just return it
+        }
+        arr[n]=solve(arr,n-1)+solve(arr,n-2);
+        return arr[n];
+    }
+}
