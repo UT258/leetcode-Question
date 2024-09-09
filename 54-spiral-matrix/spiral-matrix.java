4c2d06dd2cca56f041fd5c8e1c4022228f814e51
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-09-09 19:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (9.21%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,47 +1,57 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer>list=new ArrayList<>();
-        int r=matrix.length;
-        int c=matrix[0].length;
-        int top=0;
-        int bottom=r-1;
-        int left=0;
-        int right=c-1;
-        while(top<=bottom && left<=right)
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int top = 0;
+        int bottom = r - 1;
+        int left = 0;
+        int right = c - 1;
+        int idx = 0;
+        while (top <= bottom && left <= right)
+
         {
-            //now print the first row
-            for(int i=left;i<=right;i++)
-            {
-                list.add(matrix[top][i]);
+            // now print the first row
+            if (idx == 0) {
+                for (int i = left; i <= right; i++) {
+                    list.add(matrix[top][i]);
+                }
+
+                // since first row done move the bootom
+                top++;
             }
-            //since first row done move the bootom
-            top++;
-            //for top to down
-            for(int i=top;i<=bottom;i++)
-            {
-                list.add(matrix[i][right]);//col will be right
+            // for top to down
+            if (idx == 1) {
+                for (int i = top; i <= bottom; i++) {
+                    list.add(matrix[i][right]);// col will be right
+                }
+                right--;
             }
-            right--;
-            //for right to left
-          if (top <= bottom) {
+            // for right to left
+            if (idx == 2) {
                 // Move left across the bottom row
                 for (int i = right; i >= left; i--) {
                     list.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            
-            if (left <= right) {
+
+            if (idx == 3) {
                 // Move up the left column
                 for (int i = bottom; i >= top; i--) {
                     list.add(matrix[i][left]);
                 }
                 left++;
             }
+            idx++;
+            if (idx == 4)
+
+            {
+                idx = 0;
+            }
 
         }
         return list;
-        
+
     }
 }
