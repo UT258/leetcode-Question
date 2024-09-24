2123afacbaeef9279f7739d8ba246621f7aeb619
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-09-24 10:41:14 +0530

    Time: 92 ms (46.75%) | Memory: 55.4 MB (79.45%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,31 +1,39 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashMap<String,Integer>map=new HashMap<>();
-        for(int n:arr1)
-        {
-            String temp=Integer.toString(n);
-            String prefix="";
-            for(char c:temp.toCharArray())
-            {
-                prefix+=c;//added in the prefix
-                map.put(prefix,map.getOrDefault(prefix,0)+1);
-
+        HashMap<Integer, Integer> prefix = new HashMap<>();
+        
+        // Store all prefixes of the first array
+        for (int val : arr1) {
+            int num = val;
+            while (num > 0) {
+                prefix.put(num, prefix.getOrDefault(num, 0) + 1);
+                num /= 10;
             }
         }
-        int max=0;
-        for(int n:arr2){
-            String temp=Integer.toString(n);
-            String prefix="";
-            for(char c:temp.toCharArray())
-            {
-                prefix+=c;//added in the prefix
-                if(map.containsKey(prefix))
-                {
-                    max=Math.max(max,prefix.length());
+
+        // Variable to track the maximum length
+        int mx = Integer.MIN_VALUE;
+
+        // Instead of creating another map, check the prefix at the moment
+        for (int val : arr2) {
+            int num = val;
+            // Count the number of digits in num
+            int len = (int) Math.log10(num) + 1;
+
+            // Generate all prefixes again
+            while (num > 0) {
+                // If prefix found, break
+                if (prefix.containsKey(num)) {
+                    break;
                 }
-
+                num /= 10;
+                // Decrease digit count as we decrease prefix
+                len--;
             }
+
+            mx = Math.max(mx, len);
         }
-        return  max;
+
+        return mx;
     }
 }
