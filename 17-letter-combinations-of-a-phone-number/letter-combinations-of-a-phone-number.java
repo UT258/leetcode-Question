f8dc6fa8b4b4f8941ca2523bc960de3adcf403d8
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-11-01 21:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.61%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,26 +1,37 @@
 class Solution {
-    String map[]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    List<String>list=new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        if(digits.isEmpty())
-
-        {
-            return new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        
+        if (digits == null || digits.length() == 0) {
+            return res;
         }
-        solve("",digits);
-        return list;
+        
+        Map<Character, String> digitToLetters = new HashMap<>();
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+        
+        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
+        
+        return res;        
     }
-    public void solve(String up ,String digits)
-    {
-        if (digits.isEmpty())
-        {
-            list.add(up);
+
+    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
+        if (idx == digits.length()) {
+            res.add(comb.toString());
             return;
         }
-        int digit =digits.charAt(0)-'0';
-       for(char ch:map[digit-1].toCharArray())
-       {
-        solve(up+ch,digits.substring(1));
-       }
-    }
+        
+        String letters = digitToLetters.get(digits.charAt(idx));
+        for (char letter : letters.toCharArray()) {
+            comb.append(letter);
+            backtrack(digits, idx + 1, comb, res, digitToLetters);
+            comb.deleteCharAt(comb.length() - 1);
+        }
+    }    
 }
