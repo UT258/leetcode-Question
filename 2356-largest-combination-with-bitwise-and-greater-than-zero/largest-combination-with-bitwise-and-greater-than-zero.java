94bd7a67b0228f2dc8154f207d93a903468dbf41
utkarsh singh <83574818+UT258@users.noreply.github.com>
2024-11-07 13:17:28 +0530

    Time: 32 ms (74.60%) | Memory: 56.2 MB (87.65%) - LeetSync

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestCombination(int[] candidates) {
+    //to make the bit the >0 just check all the bits 
+    int bitcount[]=new int [24];
+    //calculate no of bits req to represent the number
+    //log2()+1
+    //it will take around 24 
+    int max=0;
+    for(int i=0;i<24;i++)
+    {
+        for(int num :candidates)
+
+        {
+            //we will find the number of set bit for the i the pos of every number 
+            if((num &  (1<<i))!=0)
+
+            {
+                bitcount[i]++;
+            }
+        }
+       //here i will have the bit count for this pos
+       max=Math.max(max,bitcount[i]);
+    }
+    return max;
+        
+    }
+}
