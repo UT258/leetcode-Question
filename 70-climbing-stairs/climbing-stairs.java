26cfbdd4c29bc4135ccc1227c00c2453e54996e9
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-01-05 14:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (71.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-       //bottom up start building soluttion from the base case
-       int dp[]=new int [n+1];
-       //since this is one based indexing
-       Arrays.fill(dp,-1);
-     return  solve(n,dp);
-        
-    }
-    public int solve(int n,int dp[])
-    {
+        int dp[]=new int [n+1];
         if(n<=2)
-        {   dp[n]=n;
-            return n;
-        }
-        if(dp[n]!=-1)
         {
-            //already have the answer 
-            return dp[n];
+            return n;//if it is smaller no need to find the anser
         }
-        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+
+        }
         return dp[n];
+        
     }
 }
