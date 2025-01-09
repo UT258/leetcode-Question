dafecd9aaa3e4e8ab65ce87edf07ec153cd4b9c2
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-01-09 17:41:10 +0530

    Time: 11 ms (6.21%) | Memory: 45.7 MB (5.10%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,28 +1,63 @@
 class Solution {
-    public int prefixCount(String[] words, String pref) {
-        int count = 0;
-        for (String s : words) {
-            if (Startswith(s, pref)) {
-                count++;
-            }
-        }
-        return count;
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        int prefixCount = 0;
 
+        public boolean contains(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void put(char ch, TrieNode node) {
+            children.put(ch, node);
+        }
+
+        public TrieNode get(char ch) {
+            return children.get(ch);
+        }
     }
 
-    public boolean Startswith(String words, String s) {
-        if(s.length()>words.length())
-        {
-            return false;
+    class Trie {
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
         }
-        // move till the prefix length and if they are equal or not
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != words.charAt(i)) {
-                return false;
+
+        // Insert a word into the Trie
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                if (!node.contains(ch)) {
+                    node.put(ch, new TrieNode());
+                }
+                node = node.get(ch);
+                node.prefixCount++; // Increment prefix count at each node
             }
-
         }
 
-        return true;
+        // Count words with a given prefix
+        public int countWordsWithPrefix(String prefix) {
+            TrieNode node = root;
+            for (char ch : prefix.toCharArray()) {
+                if (!node.contains(ch)) {
+                    return 0; // Prefix not found
+                }
+                node = node.get(ch);
+            }
+            return node.prefixCount; // Return the count of words with this prefix
+        }
+    }
+
+    public int prefixCount(String[] words, String pref) {
+        // Initialize Trie
+        Trie trie = new Trie();
+
+        // Insert all words into the Trie
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        // Return the count of words with the given prefix
+        return trie.countWordsWithPrefix(pref);
     }
 }
