f42a14d147b457fbb9a888a890dc807ac3082fd4
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-02-09 12:49:53 +0530

    Time: 40 ms (55.00%) | Memory: 58.3 MB (50.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -1,27 +1,15 @@
 class Solution {
     public long countBadPairs(int[] nums) {
-        long c=0;
-
-         // (nums[i] - i != nums[j] - j). equation can be like this
-         //we can count the number of frquency
-         for(int i=0;i<nums.length;i++)
-         {
-            nums[i]=nums[i]-i;
-         }
-         HashMap<Integer,Integer>map=new HashMap<>();
-         for(int i=0;i<nums.length;i++)
-         {
-            if(map.containsKey(nums[i]))
-            {
-                //if there is a number that is present in the lhs
-                //excluding the number
-              c+=i-map.get(nums[i]);  
-            }
-            else{
-                c+=i;
-            }
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-         }
-         return c;
+        long n = nums.length;
+        long goodPairs=0;
+        long totalPairs = n*(n-1)/2;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+           int val = i - nums[i];
+           int prevCount = map.getOrDefault(val,0);
+           goodPairs += prevCount;
+           map.put(val, map.getOrDefault(val,0)+1);
+        }
+        return totalPairs - goodPairs;
     }
 }
