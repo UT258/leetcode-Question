3d143c157c1199602ae70b6727235f30840aba6f
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-03-01 22:21:06 +0530

    Time: 2 ms (50.33%) | Memory: 43.6 MB (21.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,38 +1,29 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-
+    List<String> ans=new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        solve("", n);
+        solve("",0,0,n);
         return ans;
-    }
 
-    public boolean isvalid(String s) {
-        int sum = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                sum++;
-            } else {
-                sum--;// this means its a closing bracket it can be closed
-            }
-            if (sum < 0) {
-                return false;
-            }
-        }
-        return sum == 0;
+    
     }
-
-    public void solve(String s, int n) {
-        if (s.length() == 2 * n) {
-            if (isvalid(s))
+    public  void solve(String  s,int open , int close , int n)
+    {
+        if(s.length()== 2*n)
+        {
             ans.add(s);
-                return;
-
+            return;
         }
-        s += '(';
-        solve(s, n);
-        s=s.substring(0, s.length() - 1);
-        s += ')';
-        solve(s, n);
+        if(open <n)
+        {
+            s+='(';
+            solve(s,open+1,close,n);
+            s=s.substring(0,s.length()-1);//remove the last added bracket 
+        }
+        if(close<open)
+        {
+            s+=')';
+            solve(s,open,close+1,n);
+        }
 
     }
 }
