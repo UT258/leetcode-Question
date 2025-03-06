66ec8dc6f34708bb60f6a736642a9dfeef9e93fb
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-03-06 18:17:40 +0530

    Time: 13 ms (73.47%) | Memory: 56.6 MB (69.39%) - LeetSync

--- 2784-power-of-heroes/power-of-heroes.java
+++ 2784-power-of-heroes/power-of-heroes.java
@@ -1,26 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int sumOfPower(int[] nums) {
-        int mod = 1_000_000_007;
+        long mod = (long) 1e9 + 7, pre = 0, res = 0;
         Arrays.sort(nums);
-        
-        long res = 0, prefixSum = 0; 
-        
-        for (int i = 0; i < nums.length; i++) {
-            long minStrength = nums[i];  // The current element acts as min in some subsets
-            long maxContribution = (long) nums[i] * nums[i] % mod; 
-            
-            // Compute the total power using previously seen elements
-            long currentPower = maxContribution * (minStrength + prefixSum) % mod;
-            
-            // Add to result
-            res = (res + currentPower) % mod;
-            
-            // Update prefix sum
-            prefixSum = (prefixSum * 2 + nums[i]) % mod;
+        for (long x : nums) {
+            res = (res + (x * x % mod) * x % mod + (x * x % mod) * pre % mod) % mod;
+            pre = (pre * 2 + x) % mod;
         }
-        
         return (int) res;
     }
 }
