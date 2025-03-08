0c9f459efd4345395c493bb127db0f45cbc46591
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-03-08 11:37:31 +0530

    Time: 1 ms (92.10%) | Memory: 41.6 MB (58.45%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,20 +1,31 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int ans=Integer.MAX_VALUE;
-        //take all the substring and  the
-        
-        for(int i=0;i<=blocks.length()-k;i++)
-              //n-k cuz does not go out of bound 
-        {   int white=0; //no of white for each k len substring
-            for(int j=i;j-i+1<=k;j++)
-            {
-               if(blocks.charAt(j)=='W')
-               {
+        int i = 0;
+        int j = 0;
+        int min = Integer.MAX_VALUE;
+        int white = 0;
+        while (j < blocks.length()) {
+            if (blocks.charAt(j) == 'W') {
                 white++;
-               }
             }
-            ans=Math.min(ans,white);
+            if (j - i + 1 == k) {
+                // if the length ==k then take the no of whites in the current substring
+                min = Math.min(min, white);
+            }
+            if (j - i + 1 > k) {
+                // we have to reduce the window size
+                // but if the char is white then we need to decrease white count as well
+                if (blocks.charAt(i) == 'W') {
+                    white--;// reduce the white count
+                    min = Math.min(min, white);
+
+                }
+                i++;
+
+            }
+            j++;
         }
-        return ans;
+        return min;
+
     }
 }
