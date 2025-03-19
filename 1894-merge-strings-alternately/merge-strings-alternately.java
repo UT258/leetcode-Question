7c95fc4ee9f1f276ff97ea859e8de407acbc21a8
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-03-19 15:11:36 +0530

    Time: 1 ms (83.00%) | Memory: 42.3 MB (24.99%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,33 +1,26 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        // Using two pointers and a boolean variable to insert alternately
-        boolean alternate = true; // for alternate insertion
-        StringBuilder ans = new StringBuilder();
-        // Take two pointers i and j
-        int i = 0; // pointer for word1
-        int j = 0; // pointer for word2
-        
-        while (i < word1.length() && j < word2.length()) {
-            if (alternate) {
-                ans.append(word1.charAt(i));
-                i++;
-            } else {
-                ans.append(word2.charAt(j)); // Corrected to use j
-                j++;
-            }
-            alternate = !alternate;
+        StringBuilder ans=new StringBuilder();
+        int i=0;
+        int j=0;
+        while(i<word1.length()&& j<word2.length())
+        {
+            ans.append(word1.charAt(i));
+            ans.append(word2.charAt(j));
+            i++;
+            j++;
         }
-        
-        // Add the remaining characters in the answer
-        while (i < word1.length()) {
+        //if we OUT OF loop add the remaing 
+        while(i<word1.length())
+        {
             ans.append(word1.charAt(i));
             i++;
         }
-        while (j < word2.length()) {
+         while(j<word2.length())
+        {
             ans.append(word2.charAt(j));
             j++;
         }
-        
-        return ans.toString();   
+        return ans.toString();
     }
 }
