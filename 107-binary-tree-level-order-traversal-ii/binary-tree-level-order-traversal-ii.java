281b00b8dffc3588cec4c529671a731bc5d6cf11
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-05-24 17:47:02 +0530

    Time: 1 ms (94.54%) | Memory: 43.2 MB (31.61%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>>ans=new ArrayList<>();
+        if(root==null)
+        {
+            return ans;//return empty list 
+        }
+        Queue<TreeNode>q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            ArrayList<Integer>level=new ArrayList<>();
+            //to store the level
+            int levelsize=q.size();
+            for(int i=0;i<levelsize;i++)
+            {
+                TreeNode current=q.poll();
+                level.add(current.val);
+                if(current.left!=null)
+                {
+                    q.add(current.left);
+                }
+                if(current.right!=null)
+                {
+                    q.add(current.right);
+                }
+                
+
+            }
+             //add it in the answer
+             ans.add(level);
+        }
+        return ans;
+        
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        List<List<Integer>>ans=levelOrder(root);
+        Collections.reverse(ans);
+        return ans;
+    }
+}
