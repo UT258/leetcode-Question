bb14412621d7b0c3ebaa59e133362e1ac52f28d8
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-06-20 22:03:19 +0530

    Time: 19 ms (40.67%) | Memory: 45.3 MB (25.39%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,64 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        final int max=Integer.MAX_VALUE-1; //-1 so that overflow when i add 1
-        int n=coins.length;
-        int dp[][]=new int [n+1][amount+1];
-        //fill the base case first 
-        for(int i=0;i<=amount;i++)
-        {
-            dp[0][i]=max; //how many sum you can form when  you have 0 element
-            //infinity
-            
+    int n = coins.length;
+    int INF = Integer.MAX_VALUE - 1;
+
+    int[] prev = new int[amount + 1];
+    int[] curr = new int[amount + 1];
+
+    // Base case: with 0 coins, all amounts > 0 are impossible
+    Arrays.fill(prev, INF);
+    prev[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        curr[0] = 0; // 0 amount needs 0 coins
+        for (int j = 1; j <= amount; j++) {
+            if (coins[i - 1] <= j) {
+                int take = 1 + curr[j - coins[i - 1]];  // Use curr here for unlimited coins
+                int notake = prev[j];
+                curr[j] = Math.min(take, notake);
+            } else {
+                curr[j] = prev[j];
+            }
         }
-        for(int i=0;i<=coins.length;i++)
-        {
-            dp[i][0]=0; //how many coins you need to form 0 
-            
-        }
-         for(int i=1;i<=coins.length;i++){
-          for (int j=1;j<=amount;j++){
-              if(coins[i-1]<=j)
-              {  //take and no take case
-                  int take=1+dp[i][j-coins[i-1]];//if i take it since we have unlimited supply when we decide to pick we may pick it again
-                  int notake=dp[i-1][j];
-               dp[i][j]=Math.min(take,notake);
-
-              }
-              else{
-                  dp[i][j]=dp[i-1][j];//we cannot take it since it is greater
-              }
-          }
-
-      }
-      int ans=dp[coins.length][amount];
-      return  ans==Integer.MAX_VALUE-1?-1:ans;
-        
-        
-
-
+        // Move current row to prev for next iteration
+        prev = curr.clone();
     }
 
-    static public int coins(int[] coins, int amount, int n,int [][]dp) {
-        if(amount==0)
-        {
-            return 0;//if amount is 0 then no coins are needed
-        }
-        if (n == 0) {
+    return prev[amount] == INF ? -1 : prev[amount];
+}
 
-            return Integer.MAX_VALUE-1;
-        }
-        if(dp[n][amount]!=-1)
-        {
-            return dp[n][amount];
-        }
-        if(coins[n-1]<=amount)
-        {   //if i decide to pick it then there is chance i will pick it again  we wil not reduce the size
-            //then i can take it
-            return dp[n][amount]=Math.min(1+coins(coins,amount-coins[n-1],n, dp),coins(coins,amount,n-1, dp));
-
-        }
-        else{
-            return  dp[n][amount]=coins(coins,amount,n-1, dp); //if i dont pick it then i will reduce the size
-        }
-    }
 }
