7ffb7907edbc2c23719a70e5d5ddad9eed0cb857
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-06-22 21:19:10 +0530

    Time: 20 ms (74.71%) | Memory: 51.2 MB (30.13%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,10 +3,25 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         int dp[][] = new int[m + 1][n + 1];
-        for (int temp[] : dp) {
-            Arrays.fill(temp, -1);
+        //initialization
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0; //if text2 is empty then lcs is 0
         }
-        return solve(text1, text2, text1.length(), text2.length(), dp);
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0; //if text1 is empty then lcs is 0
+
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+
+        }
+        return dp[m][n];
 
     }
 
