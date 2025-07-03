c0e261e8925f7d1e946c93a0714f1a4bd4d78758
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-03 19:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (81.85%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,18 +1,6 @@
 class Solution {
     public char kthCharacter(int k) {
-        String word = "a";
-        String gen = "a";
-
-        for (int i = 1; gen.length() <= k; i++) {
-            StringBuilder newPart = new StringBuilder();
-            for (int j = 0; j < word.length(); j++) {
-                char shifted = (char)(word.charAt(j) + 1);
-                newPart.append(shifted);
-            }
-            word += newPart.toString();
-            gen += newPart.toString();
-        }
-
-        return gen.charAt(k-1);
+        return (char) ('a' + Integer.bitCount(k - 1));
+        
     }
 }
