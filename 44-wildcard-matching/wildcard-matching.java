e62ec5a916c4a93da69c9a3937c780fc54035fe9
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-05 10:55:46 +0530

    Time: 19 ms (62.11%) | Memory: 44.6 MB (84.50%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,8 +2,32 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length();
         int n = p.length();
-        Boolean[][] dp = new Boolean[m + 1][n + 1];
-        return solve(s, p, m, n, dp);
+        
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        dp[0][0] = true; // both strings empty
+
+        // pattern matches empty string only if all chars are '*'
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        // fill DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == sc || pc == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 
     public boolean solve(String s, String p, int i, int j, Boolean[][] dp) {
