4610cbaadce309f72862a4de200dbec998236e71
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-09 22:05:26 +0530

    Time: 3 ms (32.44%) | Memory: 46.8 MB (88.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,39 +1,53 @@
 class Solution {
-    public static void dfs(boolean vis[], int current,int  grid[][]) {
-        if(vis[current])
+    private void dfs(ArrayList<ArrayList<Integer>>adj,boolean vis[],int i)
+    {
+        vis[i]=true;//mark it visited
+        //go to neighbours
+        for(Integer n:adj.get(i))
         {
-            return;
-        }
-        vis[current] = true;
-        // find the neighbours
-
-        for (int i=0;i<grid[0].length;i++) {
-            if (grid[current][i]==1) {
-                dfs(vis, i, grid);
+            if(!vis[n])
+            {
+                dfs(adj,vis,n);
             }
         }
     }
+    public int findCircleNum(int[][] isConnected) {
+      
+     //create a adjacency list
+     ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+     int v=isConnected.length;
 
-    public int findCircleNum(int[][] isconnected) {
-        if(isconnected.length==0)
-        {
-            return 0;
-        }
-    
-     // now main program 
-     boolean vis[]=new boolean[isconnected.length];
-     int count=0;
-     for(int i=0;i<isconnected.length;i++)
+     for(int i=0;i<v;i++)
      {
-        if(vis[i]==false)
-        {
-            dfs(vis,i,isconnected);
-            count++;
-        }
-     
+      adj.add(new ArrayList<>());//since you cannot add anything to null 
      }
+     //conver adj matrix to list
+     for(int i=0;i<v;i++)
+     {
 
-    return count;
-        
+        for(int j=0;j<v;j++)
+        {
+            //since undirected  there will 
+            if(isConnected[i][j]==1 && i!=j){
+                //if it is 1 then there is edge between 0 and 1
+                adj.get(i).add(j);
+                adj.get(j).add(i);
+                
+            }
+        }
+     }
+     //now we have the adj list
+     boolean vis[]=new boolean [v];
+     int c=0;
+
+     for(int i =0;i<v;i++)
+     {
+         if(!vis[i])
+         {
+            dfs(adj,vis,i);
+            c++;
+         }
+     }
+     return c;
     }
 }
