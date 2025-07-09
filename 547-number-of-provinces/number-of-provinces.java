8f83379282ba17e76c77cae6a2085d9304e8ade8
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-09 22:59:39 +0530

    Time: 3 ms (32.44%) | Memory: 47.4 MB (74.28%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,53 +1,75 @@
 class Solution {
-    private void dfs(ArrayList<ArrayList<Integer>>adj,boolean vis[],int i)
-    {
-        vis[i]=true;//mark it visited
-        //go to neighbours
-        for(Integer n:adj.get(i))
-        {
-            if(!vis[n])
-            {
-                dfs(adj,vis,n);
+
+    class Dsu {
+        ArrayList<Integer> rank = new ArrayList<>();
+        ArrayList<Integer> parent = new ArrayList<>();
+
+        public Dsu(int n) {
+            rank.clear();
+            parent.clear();
+            for (int i = 0; i <= n; i++) {
+                //making in gen graph 0 as well as 1 base index
+                rank.add(0);
+                parent.add(i);
             }
+
+        }
+
+        public int findUpar(int node) {
+            if (node == parent.get(node)) {
+                //this is mean we find that is ultimate parent of itself
+                return node;
+            }
+            //path compression
+            parent.set(node, findUpar(parent.get(node)));
+            return parent.get(node);
+        }
+
+        public void unionByrank(int u, int v) {
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
+            //connec the smaller one to larger one 
+            if (rank.get(ulp_u) < rank.get(ulp_v))
+
+            {
+                parent.set(ulp_u, ulp_v);
+
+            }
+            if (rank.get(ulp_u) > rank.get(ulp_v))
+
+            {
+                parent.set(ulp_v, ulp_u);
+
+            } else {
+                //they are equal then rank will increase
+                //since rank are equal connecct anyone
+                parent.set(ulp_u, ulp_v);
+                rank.set(ulp_v, rank.get(ulp_v) + 1);
+            }
+
         }
     }
+
     public int findCircleNum(int[][] isConnected) {
-      
-     //create a adjacency list
-     ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
-     int v=isConnected.length;
-
-     for(int i=0;i<v;i++)
-     {
-      adj.add(new ArrayList<>());//since you cannot add anything to null 
-     }
-     //conver adj matrix to list
-     for(int i=0;i<v;i++)
-     {
-
-        for(int j=0;j<v;j++)
-        {
-            //since undirected  there will 
-            if(isConnected[i][j]==1 && i!=j){
-                //if it is 1 then there is edge between 0 and 1
-                adj.get(i).add(j);
-                adj.get(j).add(i);
-                
+        // now using the disjoint set
+        //create dsu
+        int v = isConnected.length;
+        Dsu dsu = new Dsu(v);
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    //there is a edge between u and v
+                    dsu.unionByrank(i, j);
+                }
             }
         }
-     }
-     //now we have the adj list
-     boolean vis[]=new boolean [v];
-     int c=0;
+        int count = 0;
+        for (int i = 0; i <v; i++) {
+            if (dsu.findUpar(i) == i) {
+                count++;
+            }
+        }
+        return count;
 
-     for(int i =0;i<v;i++)
-     {
-         if(!vis[i])
-         {
-            dfs(adj,vis,i);
-            c++;
-         }
-     }
-     return c;
     }
 }
