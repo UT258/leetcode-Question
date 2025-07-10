816c81ee4ced31a9234de686ee70a08643c29281
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-10 18:02:52 +0530

    Time: 632 ms (18.29%) | Memory: 41.6 MB (69.39%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,30 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0, zeroCount = 0;
-        for (int num : nums) {
-            sum += num;
-            if (num == 0) zeroCount++;
-        }
-
-        if ((sum + target) % 2 != 0 || sum < Math.abs(target)) return 0;
-
-        int s1 = (sum + target) / 2;
-        int ways = countSubsetsIgnoringZeros(nums, s1);
-
-        // Each zero can be +0 or -0, contributing to 2^zeroCount combinations
-        return (int)(ways * Math.pow(2, zeroCount));
+        return helper(nums, target, 0, 0);
     }
 
-    public int countSubsetsIgnoringZeros(int[] nums, int target) {
-        int[] dp = new int[target + 1];
-        dp[0] = 1;
+    private int helper(int[] arr, int target, int idx, int sum) {
+        if (idx >= arr.length)
+            return sum == target ? 1 : 0;
+        
+        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
+        int add = helper(arr, target, idx + 1, sum + arr[idx]);
 
-        for (int num : nums) {
-            if (num == 0) continue;
-            for (int j = target; j >= num; j--) {
-                dp[j] += dp[j - num];
-            }
-        }
-        return dp[target];
+        return subtract + add;
     }
 }
