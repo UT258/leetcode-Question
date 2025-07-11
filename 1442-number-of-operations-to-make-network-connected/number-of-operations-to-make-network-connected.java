0a48e0debe95dad8af4157fdc763a8cc9bc20fdb
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-11 10:55:54 +0530

    Time: 5 ms (76.02%) | Memory: 60.8 MB (92.03%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,66 +1,83 @@
 class Solution {
+    class Dsu{
+        int parent [];
+        int rank[];
+        public Dsu(int n)
+        {
+            parent=new int[n+1];
+            rank=new int [n+1];
+            for(int i=0;i<=n;i++)
+            {
+                parent[i]=i;//parent of itself
+                rank[i]=0;
+            }
+        }
+            public int findUpar(int node)
+            {
+                //we have to return the ulimate parent
+                if(parent[node]==node)
+                {
+                    return node;
+                }
+                parent[node]=findUpar(parent[node]);
+                return parent[node];
+            }
+            public void unionbyRank(int u,int v)
+            {
+                int ulp_u=findUpar(u);
+                int ulp_v=findUpar(v);
+                if(rank[ulp_u]<rank[ulp_v])
+                {
+                    //connect smaller to larger
+                    parent[ulp_u]=ulp_v;
+                    //new parent of the smaller one will the larger one
+
+
+                }
+                else if (rank[ulp_u]>rank[ulp_v])
+                {
+                    parent[ulp_v]=ulp_u;
+                }
+                else{
+                    //here rank are equal no mattter what you connect
+                     parent[ulp_v]=ulp_u;
+                     //u->v
+                     rank[ulp_u]++;
+                    
+                }
+            }
+        }
+    
     public int makeConnected(int n, int[][] connections) {
-        // Check if there are enough connections
-        if(connections.length < n - 1){
-            return -1;
-        }
-        
-        DisjointSet dsu = new DisjointSet(n);
-        for(int[] connection : connections){
-            dsu.unionBySize(connection[0], connection[1]);
-        }
-        
-        // The number of operations needed is the number of connected components minus one
-        return dsu.getCount() - 1;
-    }
-}
-
-class DisjointSet {
-    private int[] parent;
-    private int[] size;
-    private int count; // Number of connected components
-
-    public DisjointSet(int n) {
-        parent = new int[n];
-        size = new int[n];
-        count = n; // Initially, each node is its own component
-        
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
+         /*
+         to connect two component we need n-1 edges 
+         so just find the no of component so to make the whole graph connected 
+         we need n-1 edges that will be min 
+         */
+         if (connections.length < n - 1) {
+        // Not enough cables to connect all computers
+        return -1;
     }
 
-    public int findUltimateParent(int node){
-        if(parent[node] != node){
-            parent[node] = findUltimateParent(parent[node]); // Path compression
+         Dsu dsu=new Dsu(n);
+         int connected=0;
+        for(int temp[]:connections)
+        {
+            //there is a edge between u and v
+            int u=temp[0];
+            int v=temp[1];
+            dsu.unionbyRank(u,v);
         }
-        return parent[node];
-    }
-
-    public void unionBySize(int u, int v){
-        int ultU = findUltimateParent(u);
-        int ultV = findUltimateParent(v);
-        
-        if(ultU == ultV){
-            // Already in the same component, this is a redundant connection
-            return;
-        }
-        
-        // Union by size
-        if(size[ultU] < size[ultV]){
-            parent[ultU] = ultV;
-            size[ultV] += size[ultU];
-        }
-        else{
-            parent[ultV] = ultU;
-            size[ultU] += size[ultV];
+        //now to find the no of the component
+        for(int i=0;i<n;i++)
+        {
+            if(dsu.findUpar(i)==i)
+            {
+                //then its a component
+                connected++;
+            }
         }
         
-        count--; // After union, the number of components decreases by one
-    }
-
-    public int getCount(){
-        return count;
+        return connected -1;
     }
 }
