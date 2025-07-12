70cb00f5efc14913e002324652bbe3e606fab7d5
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-12 21:53:18 +0530

    Time: 4 ms (37.82%) | Memory: 44.3 MB (40.17%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        //time o(n) space O(n) for stack and map
+        int ans[] = new int[nums1.length];
+        int greater[] = nge(nums2);
         HashMap<Integer, Integer> map = new HashMap<>();
-        // Map every element in nums2 to its index
         for (int i = 0; i < nums2.length; i++) {
             map.put(nums2[i], i);
         }
-        
-        int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            int min = -1;
-            int j = map.get(nums1[i]); // Get the index of nums1[i] in nums2
-            for (j = j + 1; j < nums2.length; j++) { // Start from the next index
-                if (nums2[j] > nums1[i]) { // Find the next greater element
-                    min = nums2[j];
-                    break; // Exit the loop after finding the next greater element
-                }
+            ans[i] = greater[map.get(nums1[i])];//first it will  check the map and get the index of the element 
+        }
+        return ans;
+
+    }
+
+    private int[] nge(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        int ans[] = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums[i]) {
+                st.pop();
+
             }
-            ans[i] = min; // Assign the result to the answer array
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i]);
         }
         return ans;
     }
