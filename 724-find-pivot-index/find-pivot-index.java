ba7dd32aa74ee9ddf423f0166c4b6654c49049bb
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-12 15:00:31 +0530

    Time: 1 ms (92.91%) | Memory: 46 MB (18.54%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int prefix[]=new int [n];
+        int suffix[]=new int [n];
+        prefix[0]=nums[0];
+        suffix[n-1]=nums[n-1];//last element 
+        for(int i=1;i<nums.length;i++)
+        {
+            prefix[i]=prefix[i-1]+nums[i];
+            suffix[nums.length-1-i]=suffix[nums.length-i]+nums[nums.length-i-1];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(suffix[i]==prefix[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
