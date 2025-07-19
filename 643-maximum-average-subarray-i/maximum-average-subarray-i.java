4e9d18ab57394ef7263dfd122776aafb59d99a47
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-07-19 18:47:11 +0530

    Time: 6 ms (18.53%) | Memory: 56.2 MB (65.40%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,22 +1,29 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int max = Integer.MIN_VALUE;
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        while (j < nums.length) {
-            sum += nums[j];
+        //create a avg 
+        double avg=Double.NEGATIVE_INFINITY;
+        int i=0;
+        int j=0;
+        double sum=0;
+        while(j<nums.length)
+        {
+            sum +=nums[j];
+            //update the size if it equal to the size of the window
+            int l=j-i+1;
+            if(l==k)
+            {
+             //calulate the avg 
+             double CurrAvg=sum/l;
+               avg=Math.max(avg ,CurrAvg);
+               //move the window now already find the answer
+               sum-=nums[i];
+               i++;
 
-            // Check if we have reached the window size
-            if (j - i + 1 == k) {
-                max = Math.max(max, sum);
-                sum -= nums[i]; // Slide the window forward by removing the element at index i
-                i++;
             }
-            j++;
-
+            
+            j++;//increase overall window
         }
-        return (double)max / k;
-
+     return avg;
+        
     }
 }
