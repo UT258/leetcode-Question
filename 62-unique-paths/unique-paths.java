92ca00d549bbe7c582060624a074e1eab0fd53e8
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-05 17:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (88.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,33 @@
 class Solution {
-    public static int solve(int m,int n,int dp[][])
-    {
-     if(m==0 || n==0)
-     {
-        return 1;
-     }
-     if(n<1 || m<1)
-     {
-        return 0;
-     }
-     if(dp[m][n]!=-1)
-     {
-        return dp[m][n];
-     }
-    
-     int top =solve(m,n-1,dp);
-     int right=solve(m-1,n,dp);
-     dp[m][n]=top+right;
-     return    dp[m][n];
-    }
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int [m+1][n+1];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        int dp[][]=new int [m][n];
+        for(int temp[]:dp)
+        {
+            Arrays.fill(temp,-1);
         }
+
         return solve(m-1,n-1,dp);
+        
+    }
+    public static int solve(int m,int n,int [][]dp)
+
+    {
+        if(m== 0 && n==0)
+        {
+            //reach the destination
+            return 1;
+        }
+        int left=0;
+        int up=0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(n>0)
+        {
+            left+=solve(m,n-1,dp);
+        }
+        if(m>0)
+        {
+            up+=solve(m-1,n,dp);
+        }
+        return dp[m][n]= left+up;
     }
 }
