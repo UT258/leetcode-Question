cc12f2b22922b051e76df3555f5cc0320c250b5c
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-06 16:51:16 +0530

    Time: 12 ms (98.25%) | Memory: 49.5 MB (78.63%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,26 +1,68 @@
 class MyHashSet {
-    boolean arr[];
+    private class Node{
+        int key;
+        Node next;
+
+        Node(int key){
+            this.key = key;
+            this.next = null;
+        }
+    }
+
+    private final Node[] set;
+    private final int SIZE = 1000;
+
     public MyHashSet() {
-        arr=new boolean [1000001];
+        set = new Node[SIZE];
+    }
+
+    private int getIndex(int key){
+        return key % SIZE;
     }
     
     public void add(int key) {
-        arr[key]=true;
+        int index = getIndex(key);
+        Node head = set[index];
+
+        Node curr = head;
+        while(curr != null){
+            if(curr.key == key){
+                return;
+            }
+            curr = curr.next;
+        }
+
+        Node newNode = new Node(key);
+        newNode.next = head;
+        set[index] = newNode;
     }
     
     public void remove(int key) {
-        arr[key]=false;
+        int index = getIndex(key);
+        Node curr = set[index];
+        Node prev = null;
+        while(curr != null){
+            if(curr.key == key){
+                if(prev == null){
+                    set[index] = curr.next;
+                }else{
+                    prev.next = curr.next;
+                }
+            }
+            prev = curr;
+            curr = curr.next;
+        }
     }
     
     public boolean contains(int key) {
-        return arr[key];
+        int index = getIndex(key);
+        Node curr = set[index];
+        while(curr != null){
+            if(curr.key == key){
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
