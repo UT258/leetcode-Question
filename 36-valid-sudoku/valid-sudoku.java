aea0f73b381e5dbd4f5f1ba070045fa1f35d1e7c
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-08 17:32:51 +0530

    Time: 1 ms (99.93%) | Memory: 44.5 MB (57.26%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,57 +1,26 @@
 class Solution {
-    private boolean valid(int sr, int sc, int er, int ec, char[][] board) {
-        Set<Character> set = new HashSet<>();
-        for (int i = sr; i <= er; i++) {
-            for (int j = sc; j <= ec; j++) {
-                if (board[i][j] == '.')
-                    continue;
-                if (set.contains(board[i][j])) {
-                    return false;
-                }
-                set.add(board[i][j]);
-            }
-        }
-        return true;
-    }
-
     public boolean isValidSudoku(char[][] board) {
-        //check all the rows
-        //and check all the cols 
-        //check for rows
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
         for (int i = 0; i < 9; i++) {
-            Set<Character> set = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.')
-                    continue; //no need to check
-                if (set.contains(board[i][j])) {
+                if (board[i][j] == '.') continue;
+
+                int digit = board[i][j] - '0' - 1; // 0-based index
+                int boxIndex = (i / 3) * 3 + (j / 3); // Map to 3x3 box index (0 to 8)
+
+                if (row[i][digit] || col[j][digit] || box[boxIndex][digit]) {
                     return false;
                 }
-                set.add(board[i][j]);//just add normally
-            }
-        }
-        //check for columns
-        for (int i = 0; i < 9; i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[j][i] == '.')
-                    continue; //no need to check
-                if (set.contains(board[j][i])) {
-                    return false;
-                }
-                set.add(board[j][i]);//just add normally
+
+                row[i][digit] = true;
+                col[j][digit] = true;
+                box[boxIndex][digit] = true;
             }
         }
 
-        //check for individual matrix
-        for (int sr = 0; sr < 9; sr += 3) {
-            int er = sr + 2;
-            for (int sc = 0; sc < 9; sc += 3) {
-                int ec = sc + 2;
-                if (!valid(sr, sc, er, ec, board)) {
-                    return false;
-                }
-            }
-        }
-        return true;//none of the condition matched so it is valid
+        return true;
     }
 }
