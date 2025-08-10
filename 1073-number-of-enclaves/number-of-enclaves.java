5f205932729a5d1a8bf66b57bfdb61da19b9fdb3
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-10 19:11:17 +0530

    Time: 8 ms (74.12%) | Memory: 57.1 MB (48.05%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,59 +1,43 @@
 class Solution {
-    int c = 0;
+    int totalLand = 0;
+    int boundaryLand = 0;
+    int[] dr = {0, 0, 1, -1};
+    int[] dc = {1, -1, 0, 0};
 
-    private void dfs(int i, int j, int[][] grid, boolean vis[][], int[] dr, int[] dc)
+    private void dfs(int i, int j, int[][] grid) {
+        grid[i][j] = 0; // mark visited
+        boundaryLand++; // count this land cell
 
-    {
-        vis[i][j] = true;
-        
-        //go to negbour
-        for (int k = 0; k< 4; k++) {
+        for (int k = 0; k < 4; k++) {
             int nr = i + dr[k];
             int nc = j + dc[k];
-            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && !vis[nr][nc] && grid[nr][nc]==1) {
-                dfs(nr, nc, grid, vis, dr, dc);
+            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length 
+                && grid[nr][nc] == 1) {
+                dfs(nr, nc, grid);
             }
         }
-
     }
 
     public int numEnclaves(int[][] grid) {
-        
-    //start from boundary and connect 
-       int m=grid.length;
-       int n=grid[0].length;
-       boolean vis[][]=new boolean [m][n];
-       int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-       for(int i=0;i<m;i++)
-       {
-        for(int j=0;j<n;j++)
-        {
-           if(i==0 || j==0 || i==m-1 ||j==n-1 )
-           {  if(!vis[i][j] && grid[i][j]==1)
-             dfs(i,j,grid,vis,dx,dy);
-                
-           }
-           
-           
+        int m = grid.length, n = grid[0].length;
+
+        // Count total land first
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) totalLand++;
+            }
         }
 
-       }
-       int c=0;
-       for(int i=0;i<m;i++)
-       {
-        for(int j=0;j<n;j++)
-        {
-           if(grid[i][j]==1 && !vis[i][j])
-           {
-            c++;
-           }
-           
-           
+        // DFS from boundaries to remove connected lands
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) 
+                    && grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                }
+            }
         }
 
-       }
-
-       return c;
+        return totalLand - boundaryLand;
     }
 }
