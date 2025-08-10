29eaeadd1ee1ba73a425ce7d222e1252996a2842
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-10 18:31:15 +0530

    Time: 3 ms (32.36%) | Memory: 46.8 MB (90.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,62 +1,50 @@
 class Solution {
-
-    class Dsu {
-        int[] parent;
-        int[] rank;
-
-        public Dsu(int n) {
-            parent = new int[n];
-            rank = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 0;
-            }
-        }
-
-        public int findUpar(int node) {
-            if (parent[node] != node) {
-                parent[node] = findUpar(parent[node]); // Path compression
-            }
-            return parent[node];
-        }
-
-        public void unionByRank(int u, int v) {
-            int ulp_u = findUpar(u);
-            int ulp_v = findUpar(v);
-
-            if (ulp_u == ulp_v) return; // Already in same set
-
-            if (rank[ulp_u] < rank[ulp_v]) {
-                parent[ulp_u] = ulp_v;
-            } else if (rank[ulp_v] < rank[ulp_u]) {
-                parent[ulp_v] = ulp_u;
-            } else {
-                parent[ulp_u] = ulp_v;
-                rank[ulp_v]++;
+    private void dfs(int i,boolean vis[],int[][] grid,ArrayList<ArrayList<Integer>>adj)
+    {   if(vis[i]) return ;
+        vis[i]=true;//mark the node visited
+        for(Integer curr:adj.get(i))
+        {
+            if(!vis[curr])
+            {
+                dfs(curr,vis,grid,adj);
             }
         }
     }
-
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        Dsu dsu = new Dsu(n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    dsu.unionByRank(i, j);
-                }
+       int m=isConnected.length;
+       int n=isConnected[0].length;
+       //create adjacecy list
+       ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+       for(int i=0;i<m;i++)
+       {
+        adj.add(new ArrayList<>());//to intialize
+       }
+       for(int i=0;i<m;i++)
+       {
+        for(int j=0;j<n;j++)
+        {
+            if(isConnected[i][j]==1 && i!=j)
+            {
+                //not consider the self nodes
+                adj.get(i).add(j);
+                adj.get(j).add(i);
             }
         }
+       }
 
-        // Count the number of unique parents
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (dsu.findUpar(i) == i) {
-                count++;
+        boolean vis[]=new boolean [m];
+        int c=0;
+        for(int i=0;i<m;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,vis,isConnected,adj);
+                c++;
             }
         }
+        return c;
 
-        return count;
+       
+       
     }
 }
