713c13f188c4dfa7c5600eeaa13da0356335a4d7
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-15 11:59:51 +0530

    Time: 11 ms (98.00%) | Memory: 58.9 MB (90.02%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -1,38 +1,28 @@
 class Solution {
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        // Build adjacency list for subordinates
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+    public int numOfMinutes(int n, int headID, int[] manager, int[] inform) {
+        int[] memo = new int[n]; // Memoization array to store calculated inform times
+        
+        int max = 0;
+        for(int i = 0; i < n; i++ ){
+            max = Math.max(max, find(i, manager, inform, memo));
+        }
+
+        return max;
+    }
+
+
+    public int find(int x, int[] manager, int[] inform, int[] memo){
+        if(manager[x] == -1){//the employee is a manger so return the return time of just the manager
+            return inform[x];
         }
         
-        for (int i = 0; i < n; i++) {
-            if (manager[i] != -1) {
-                adj.get(manager[i]).add(i);
-            }
+        if(memo[x] != 0) { // If the inform time is already calculated and stored in the memo array, return it
+            return memo[x];
         }
+        //calculate the inform time of the employee and the managers of the employee
+        int time = inform[x] + find(manager[x], manager, inform, memo);
+        memo[x] = time; // Store the calculated inform time in the memo array
         
-        // BFS queue: store {employeeID, timeTakenSoFar}
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{headID, 0});
-        
-        int maxTime = 0;
-        
-        while (!q.isEmpty()) {
-            int[] front = q.poll();
-            int empID = front[0];
-            int timeSoFar = front[1];
-            
-            // Update max time
-           
-            
-            // Push all subordinates with updated time
-            for (Integer sub : adj.get(empID)) {
-                q.add(new int[]{sub, timeSoFar + informTime[empID]});
-            }
-             maxTime = Math.max(maxTime, timeSoFar);
-        }
-        
-        return maxTime;
+        return time;
     }
 }
