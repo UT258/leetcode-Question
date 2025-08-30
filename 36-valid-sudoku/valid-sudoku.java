3e66594efd6314875ffed6ae309cecbb1b18bcf9
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-08-30 10:16:56 +0530

    Time: 2 ms (77.42%) | Memory: 44.4 MB (66.47%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,26 +1,80 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9];
-        boolean[][] col = new boolean[9][9];
-        boolean[][] box = new boolean[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-
-                int digit = board[i][j] - '0' - 1; // 0-based index
-                int boxIndex = (i / 3) * 3 + (j / 3); // Map to 3x3 box index (0 to 8)
-
-                if (row[i][digit] || col[j][digit] || box[boxIndex][digit]) {
-                    return false;
-                }
-
-                row[i][digit] = true;
-                col[j][digit] = true;
-                box[boxIndex][digit] = true;
+    private boolean valid(int sr,int er, int sc, int ec,char[][] board)
+    {
+     Set<Character>set=new HashSet<>();
+     for(int i=sr;i<=er;i++)
+     {
+        for(int j=sc;j<=ec;j++)
+        {  char ch=board[i][j];
+            if(ch=='.') 
+            {
+                continue;
+            }
+            if(set.contains(ch))
+            {
+                return false;
+            }
+            else{
+                set.add(ch);
             }
         }
+     }
+     return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        //check the row 
+         int m=board.length;
+         int n=board[0].length;
+         for(int i=0;i<9;i++)
 
-        return true;
+         {  //now  each col should not contain a duplicate digit
+             Set<Character>set=new HashSet<>();
+            for(int j=0;j<9;j++)
+            {
+               char ch=board[j][i];//moving in the col
+               if(ch=='.')
+               {
+                continue;
+               }
+               if(set.contains(ch))
+               {
+                return false;
+               }
+               else{
+                set.add(ch);
+               }
+            }
+         }
+         //now check if there any duplicate int the row 
+         for(int i=0;i<9;i++){
+            Set<Character>set=new HashSet<>();
+            for(int j=0;j<9;j++)
+            {
+                char ch=board[i][j];
+                if(ch=='.')
+                {
+                    continue;//if its empty do nothing
+                }
+                if(set.contains(ch)){
+                    return false;
+                }
+                else{
+                    set.add(ch);
+                }
+            }
+         }
+         //now check the individul box
+         for(int sr=0;sr<9;sr+=3)
+         {
+            int er=sr+2;
+            for(int sc=0;sc<9;sc+=3)
+            {
+                int ec=sc+2;
+                if(!valid(sr,er,sc,ec,board)){
+                    return false;
+                }
+            }
+         }
+         return true;
     }
 }
