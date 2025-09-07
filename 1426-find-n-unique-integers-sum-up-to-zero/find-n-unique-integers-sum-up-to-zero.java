a1e6243a750502e6bd1fe097c33d6eca67c62f50
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-09-07 17:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (51.42%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] sumZero(int n) {
-        int arr[]=new int[n];
-        int a=n;
-      int  end=n/2+1;
-        for(int i=0;i<n/2;i++)
-        {
-            arr[i]=a;
-            a--;
+        int arr[] = new int[n];
+        int start = n;
+        //create half positives and half negative
+        //start from last and till half put all the positive
+        //if n is odd then half pos and half negative  and middle 0 
+        //5 4 0 -5 -4  odd length 
+        // 5 4 -4 -5 for even length mid element will not be zero 
+         //that the only way you can form the positive negative easily
+        int end = n / 2 + 1;
+        for (int i = 0; i < n / 2; i++) {
+            arr[i] = start;
+            start--;
         }
-        a=n;
-        if(n%2==0)
-        {
-             end=n/2;
+        start = n; //reset for the negatives
+        if (n % 2 == 0) {
+            end = n / 2;
+            //if is is even start putting from middle 
+            //else skip the mid
         }
-        for(int i=end;i<n;i++)
-        {
-         arr[i]=-1*a;
-         a--;
+        for (int i = end; i < n; i++) {
+            arr[i] = -1 * start;
+            start--;
         }
         return arr;
-        
+
     }
 }
