8fafc29c19709df370d6625fcdffedd5a9de2718
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-09-11 22:14:27 +0530

    Time: 2 ms (43.01%) | Memory: 57.6 MB (34.22%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,16 +1,33 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-    int size=nums.length;
-    Set<Integer>set=new HashSet<>();
-    for(int n:nums)
-    {   if(n>0 )
-        set.add(n);
+        //cyclic sort
+        sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i+1 )
+            {
+                return i+1;
+            }
+        }
+        return nums.length+1;
+      
     }
-    for(int i=1;i<=size;i++)
+    private void sort(int arr[])
     {
-        if(!set.contains(i))
-        return i;
-    }
-    return nums.length+1;
+        int i=0;
+        while(i<arr.length  )
+        {
+            int correct=arr[i]-1;
+            if(  arr[i]>0  && arr[i]<=arr.length && arr[i]!=arr[correct])
+            {
+                //if not at correct post swap with correct
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
     }
 }
