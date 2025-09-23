0df090d9074e27c5f50ba800c5f4ca30e483fb7e
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-09-23 20:00:37 +0530

    Time: 1 ms (76.03%) | Memory: 42 MB (6.38%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,35 +1,14 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-         String first[] = version1.split("\\.");
-        String sec[] = version2.split("\\.");
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-        if(first.length < sec.length) {  
-            String temp[] = new String[sec.length];
-            for(int i = 0; i < first.length; i++) {
-                temp[i] = first[i];
-            }
-            for(int i = first.length; i < sec.length; i++) {
-                temp[i] = "0";
-            }
-            first = temp;
-        }
-
-        if(sec.length < first.length) {
-            String temp[] = new String[first.length];
-            for(int i = 0; i < sec.length; i++) {
-                temp[i] = sec[i];
-            }
-            for(int i = sec.length; i < first.length; i++) {
-                temp[i] = "0";
-            }
-            sec = temp;
-        }
-
-        for(int i = 0; i < sec.length; i++) {
-            int f = Integer.parseInt(first[i]);
-            int s = Integer.parseInt(sec[i]);
-            if(f < s) return -1;
-            if(f > s) return 1;
+        int len = Math.max(v1.length, v2.length);
+        for (int i = 0; i < len; i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
         }
         return 0;
     }
