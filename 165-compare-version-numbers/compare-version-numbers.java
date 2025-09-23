dce2d245ddf05f31cc608dc807429633ab497084
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-09-23 19:49:47 +0530

    Time: 1 ms (76.03%) | Memory: 41.5 MB (67.13%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,35 +1,16 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-         String first[] = version1.split("\\.");
-        String sec[] = version2.split("\\.");
+        String[] first = version1.split("\\.");
+        String[] sec = version2.split("\\.");
 
-        if(first.length < sec.length) {  
-            String temp[] = new String[sec.length];
-            for(int i = 0; i < first.length; i++) {
-                temp[i] = first[i];
-            }
-            for(int i = first.length; i < sec.length; i++) {
-                temp[i] = "0";
-            }
-            first = temp;
-        }
+        int n = Math.max(first.length, sec.length);
 
-        if(sec.length < first.length) {
-            String temp[] = new String[first.length];
-            for(int i = 0; i < sec.length; i++) {
-                temp[i] = sec[i];
-            }
-            for(int i = sec.length; i < first.length; i++) {
-                temp[i] = "0";
-            }
-            sec = temp;
-        }
+        for (int i = 0; i < n; i++) {
+            int f = (i < first.length) ? Integer.parseInt(first[i]) : 0;
+            int s = (i < sec.length) ? Integer.parseInt(sec[i]) : 0;
 
-        for(int i = 0; i < sec.length; i++) {
-            int f = Integer.parseInt(first[i]);
-            int s = Integer.parseInt(sec[i]);
-            if(f < s) return -1;
-            if(f > s) return 1;
+            if (f < s) return -1;
+            if (f > s) return 1;
         }
         return 0;
     }
