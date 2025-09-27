ab06ff49eccd7485c4405808df01a40b4d92cfa6
utkarsh singh <83574818+UT258@users.noreply.github.com>
2025-09-27 12:33:36 +0530

    Time: 3 ms (39.73%) | Memory: 45.9 MB (43.68%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        Boolean dp[]=new Boolean [nums.length+1];
-      
-        return solve(nums,0,dp);
-    }
-    private boolean solve(int arr[],int i,Boolean dp[])
-    {
-        if(i==arr.length-1)
+        int max=0;
+        for(int i=0;i<nums.length;i++)
         {
-            return true; //able to reach the last index
+            if(i>max)
+            {
+                return false ; //if you are not able to reach this you cannot reach it
 
+            } 
+            max=Math.max(max,i+nums[i]);
         }
-          if (i >= arr.length) {
-            return false;
-        }
-        if(dp[i]!=null)return dp[i];
-        // 3) Try every possible jump length from 1 up to arr[i]
-        for (int step = 1; step <= arr[i]; step++) {
-            if (solve(arr, i + step,dp)) {
-                return dp[i]= true;
-            }
-        }
-
-        //i represent if i start from ind can i reach the last 
-        return dp[i]=false;
-
+        return true;//you can always reach
+        
     }
-    
 }
