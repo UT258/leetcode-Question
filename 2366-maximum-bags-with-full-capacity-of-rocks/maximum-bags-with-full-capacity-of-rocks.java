239d3413ceb4088b4e2bfc49bade149b98e13254
utkarsh singh <83574818+UT258@users.noreply.github.com>
2026-08-07 12:29:14 +0530

    Time: 18 ms (84.44%) | Memory: 67.7 MB (17.36%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        //find the diff 
+        int count=0;
+        for(int i=0;i<rocks.length;i++)
+        {
+            capacity[i]-=rocks[i];
+        }
+        Arrays.sort(capacity);
+        for(int i=0;i<rocks.length;i++)
+        {
+            //first try to complete the capacirt that need the min rocks first
+            if(capacity[i]<=additionalRocks)
+            {
+                //i can fill this bag completeyy
+                count++;
+                additionalRocks-=capacity[i];
+            }
+        }
+        return count;
+        
+    }
+}
